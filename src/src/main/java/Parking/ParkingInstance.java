c33a4205950cc0b323ed1051bd31bdb1811010d0
clua <clairelua8@gmail.com>
2019-11-09 15:32:28 -0800

    Add classes and methods skeleton
    Add classes and methods skeleton

---
+++ src/src/main/java/Parking/ParkingInstance.java
@@ -0,0 +1,11 @@
+package Parking;
+
+/**
+ * ParkingInstance contains parking instance object that represents each
+ * instance that a car is parked at the parking lot.
+ */
+class ParkingInstance {
+    Date date;
+    Car car;
+    String photoHash;
+}
---
+++ src/src/main/java/Parking/ParkingInstanceDatabase.java
@@ -0,0 +1,30 @@
+package Parking;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Database to store all the parking instances (each time a car is parked)
+ */
+class ParkingInstanceDatabase {
+    ArrayList<ParkingInstance> instances = new ArrayList<ParkingInstance>();
+
+    /**
+     * Get parking instances to get database
+     * 
+     * @return
+     */
+    public ArrayList<ParkingInstance> getInstances() {
+        return instances;
+    }
+
+    /**
+     * Add new parking Instances when new photos are uploaded.
+     * 
+     * @param newInstances
+     */
+    public void addInstances(ArrayList<ParkingInstance> newInstances) {
+        this.instances.add(newInstances);
+
+    }
+
+}
