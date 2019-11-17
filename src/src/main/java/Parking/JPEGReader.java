1336b3482b4214f0ecaae5a0eed13cb15e6895b0
samlau <samlaujw@gmail.com>
2019-11-17 10:44:27 -0800

    built photo and car objects

--- src/src/main/java/Parking/JPEGReader.java
+++ src/src/main/java/Parking/JPEGReader.java
@@ -17,48 +17,48 @@ import com.drew.metadata.exif.*;
 public class JPEGReader {
 
 	/**
-	 * This method extracts the original date of the files from a folder by
-	 * extracting exif data and storing them in an ArrayList of Strings.
-	 * 
+	 * createPhotos creates an ArrayList of photo objects from each image file in the designated folder.
 	 * @param path
-	 * @return ArrayList<String>
+	 * @return
 	 */
-	public ArrayList<LocalDateTime> readDates(Path path) {
-		// initializing ArrayList of LocalDateTime objects
-		ArrayList<LocalDateTime> dates = new ArrayList<LocalDateTime>();
+	public ArrayList<Photo> createPhotos(Path path) {
+		// initialize ArrayList of photo objects
+		ArrayList<Photo> photoArrayList = new ArrayList<Photo>();
 
 		try {
-			// store files from the folder location
+			// get paths to the image files in the folder
 			File[] files = path.toFile().listFiles();
 
-			// iterate through each file in the folder
+			// iterate through each image file in the folder to create a photo object
 			try {
 				for (int i = 0; i < files.length; i++) {
 					try {
+						System.out.println("Scanning photo " + (i+1) + "/" + files.length);
+
+						// get photo file path
+						String filePath = files[i].toString();
+
+						// get byte data of photo
+						Path pathToImageFile = Paths.get(filePath);
+						byte[] data = Files.readAllBytes(pathToImageFile);
+
 						// get metadata from the file and obtain Exif date and time
-						Metadata metadata = ImageMetadataReader.readMetadata(files[i]);
-						ExifSubIFDDirectory dir = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-						String dateStr = dir.getString(ExifIFD0Directory.TAG_DATETIME_ORIGINAL);
-//						System.out.println(dateStr);
-						String pattern = "yyyy:MM:dd HH:mm:ss";
-						DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-						LocalDateTime formattedDate = LocalDateTime.parse(dateStr, formatter);
-						
-//						// to get date from LocalDateTime object
-//						System.out.println(formattedDate.toLocalDate());
-//						// to get time from LocalDateTime object
-//						System.out.println(formattedDate.toLocalTime());
-						
-						// add each LocalDateTime object to ArrayList
-						dates.add(formattedDate);
+						LocalDateTime formattedDate = readDates(files[i]);
+
+						// get random UUID for this photo
+						String randomUUID = generateUUID();
+
+						// create photo object
+						Photo myPhoto = new Photo(data, formattedDate, randomUUID, filePath);
+//						System.out.println(myPhoto.getPhotoToString() + "\n");
+
+						photoArrayList.add(myPhoto);
 
 						// exception handling
 					} catch (NullPointerException e) {
 						System.out.println("File " + files[i].toString() + " doesn't have metadata.");
 					} catch (IOException e) {
 						System.out.println("File \"" + files[i].toString() + "\" cannot read metadata.");
-					} catch (ImageProcessingException e) {
-						System.out.println("File \"" + files[i].toString() + "\" format could not be determined.");
 					}
 				}
 			} catch (NullPointerException e) {
@@ -67,29 +67,61 @@ public class JPEGReader {
 		} catch (ProviderNotFoundException e) {
 			System.out.println("Given path is invalid");
 		}
-
-		return dates;
+	
+		return photoArrayList;
 	}
+
 
 	/**
-	 * buildPhotoObject creates a photo object from each photo.
+	 * This is a private method that extracts the original date of the files from a folder by
+	 * extracting exif data and storing them in an ArrayList of Strings.
 	 * 
-	 * @param args
+	 * Note:
+	 * To get date from LocalDateTime object: formattedDate.toLocalDate()
+	 * To get time from LocalDateTime object: formattedDate.toLocalTime()
+	 * 
+	 * @param file
+	 * @return
 	 */
-	public void buildPhotoObject() {
-		// TODO fill in method
-
+	private LocalDateTime readDates(File file) {
+		Metadata metadata;
+		LocalDateTime formattedDate = null;
+		try {
+			metadata = ImageMetadataReader.readMetadata(file);
+			ExifSubIFDDirectory dir = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+			String dateStr = dir.getString(ExifIFD0Directory.TAG_DATETIME_ORIGINAL);
+			//	System.out.println(dateStr);
+			String pattern = "yyyy:MM:dd HH:mm:ss";
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+			formattedDate = LocalDateTime.parse(dateStr, formatter);
+		} catch (ImageProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return formattedDate;
 	}
 
+
+	/**
+	 * This is a private helper method that generates a random uuid string for each image file
+	 * to be used as a unique identifier.
+	 * @return
+	 */
+	private String generateUUID() {
+		UUID uuid = UUID.randomUUID();
+		String randomUUIDString = uuid.toString();
+		return randomUUIDString;
+	}
+
+	
 	public static void main(String[] args) {
 		JPEGReader r = new JPEGReader();
 		Path filePath = Paths.get("src/test/java/Parking/MultipleImagesFolder/");
-		ArrayList<LocalDateTime> dates = r.readDates(filePath);
-		System.out.println(dates.toString());
+		r.createPhotos(filePath);
+	
+		
 	}
 
-	public JPEGReader() {
-
-	}
 
 }
