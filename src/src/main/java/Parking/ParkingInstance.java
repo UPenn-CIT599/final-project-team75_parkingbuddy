bf6628a410451375cf5ade54674052ab79da4238
samlau <samlaujw@gmail.com>
2019-11-17 20:10:48 -0800

    parking processor changes

--- src/src/main/java/Parking/ParkingInstance.java
+++ src/src/main/java/Parking/ParkingInstance.java
@@ -1,12 +1,52 @@
 package Parking;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 /**
  * ParkingInstance contains parking instance object that represents each
  * instance that a car is parked at the parking lot.
  */
 class ParkingInstance {
-    Date date;
-    Car car;
-    String photoHash;
+    private LocalDateTime date;
+    private Car car;
+    private String photoHash;
+    
+    public ParkingInstance(LocalDateTime date, Car car, String photoHash) {
+    	this.date = date;
+    	this.car = car;
+    	this.photoHash = photoHash;
+    }
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public Car getCar() {
+		return car;
+	}
+
+	public void setCar(Car car) {
+		this.car = car;
+	}
+
+	public String getPhotoHash() {
+		return photoHash;
+	}
+
+	public void setPhotoHash(String photoHash) {
+		this.photoHash = photoHash;
+	}
+    
+	public String parkingInstanceToString() {
+		String dateStr = date.toString();
+		String carStr = car.getLicense() + "," + car.getState();
+		String photoHashStr = photoHash;
+		String parkingInstanceStr = dateStr + "," + carStr + "," + photoHashStr;
+		
+		return parkingInstanceStr;
+	}
+    
 }
--- src/src/main/java/Parking/ParkingInstanceProcessor.java
+++ src/src/main/java/Parking/ParkingInstanceProcessor.java
@@ -1,5 +1,11 @@
 package Parking;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.ProviderNotFoundException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
  * ParkingInstanceProcessor creates an arraylist of parking instances from a
@@ -7,21 +13,65 @@ package Parking;
  */
 
 public class ParkingInstanceProcessor {
-    JPEGReader jpegReader = new JPEGReader();
-    LicenseOCR licenseOCR = new LicenseOCR();
+	
+	/**
+	 * This method creates a ParkingInstance object from two arguments: Car and Photo. 
+	 * @param car
+	 * @param photo
+	 * @return
+	 */
+	public ParkingInstance createParkingInstance(Car car, Photo photo) {
+		LocalDateTime date = photo.getCreationDate();
+		String photoHash = photo.getPhotoHash();
+		ParkingInstance parkingInstance = new ParkingInstance(date, car, photoHash);
+
+		return parkingInstance;
+	}
+
+	public ArrayList<ParkingInstance> createParkingInstanceArray(Path path) {
+		ParkingInstanceProcessor pip = new ParkingInstanceProcessor();
+		ArrayList<ParkingInstance> parkingInstanceArrayList = new ArrayList<ParkingInstance>();
+		JPEGReader r = new JPEGReader();
+		LicenseOCR ocr = new LicenseOCR(); 
+		path = Paths.get("src/test/java/Parking/MultipleImagesFolder/");
+
+		// loops through folder with image files to create from each file: (1) a Photo object, (2) a Car object, and (3) a ParkingInstance object
+		// the ParkingInstance object is stored in an ArrayList<ParkingInstance>
+		try {
+			// get paths to the image files in the folder
+			File[] files = path.toFile().listFiles();
+			// iterate through each image file in the folder to create a photo object
+			try {
+				for (int i = 0; i < files.length; i++) {
+					System.out.println("Scanning photo " + (i+1) + "/" + files.length);
+					File file = files[i];
+					Photo myPhoto = r.createPhoto(file);
+					Car myCar = ocr.createCar(myPhoto);
+					ParkingInstance parkingInstance = pip.createParkingInstance(myCar, myPhoto);
+					parkingInstanceArrayList.add(parkingInstance);
+				}
+				
+			} 
+			catch (NullPointerException e) {
+				System.out.println("The folder is empty");
+			} 
+		} 
+		catch (ProviderNotFoundException e) {
+			System.out.println("Given path is invalid");
+		}
+
+		return parkingInstanceArrayList;
+	}
 
 
-    /**
-     * createCar method creates a new Car object from the license OCR
-     */
-
-    public void addCar() {
-        //check that the license don't already exist in carsDatabase
-        // add car to carsDatabase
-        
-    }
-     
-    public ParkingInstanceProcessor() {
-
-    }
+	public static void main(String[] args) {
+		Path filePath = Paths.get("src/test/java/Parking/MultipleImagesFolder/");
+		ParkingInstanceProcessor pip = new ParkingInstanceProcessor();
+		ArrayList<ParkingInstance> parkingInstanceArr = new ArrayList<ParkingInstance>();
+		parkingInstanceArr = pip.createParkingInstanceArray(filePath);
+		for (ParkingInstance pi : parkingInstanceArr) {
+			System.out.println("Parking Instance Array: " + pi.parkingInstanceToString());
+		}
+	}
+	
 }
