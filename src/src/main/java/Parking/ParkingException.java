eaeff26df98882743fc1afbcf07942b435fd67e3
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-01 17:46:58 -0800

    edit

--- src/src/main/java/Parking/ParkingException.java
+++ src/src/main/java/Parking/ParkingException.java
@@ -1,15 +1,14 @@
 package Parking;
 
 public class ParkingException extends Exception {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    // Parameterless Constructor
-    public ParkingException() {
-    }
+	// Parameterless Constructor
+	public ParkingException() {
+	}
 
-    // Constructor that accepts a message
-    public ParkingException(String message) {
-        super(message);
-    }
+	// Constructor that accepts a message
+	public ParkingException(String message) {
+		super(message);
+	}
 }
-
