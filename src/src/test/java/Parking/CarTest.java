f9e989947f75443f986dbf2c4d55686516688dd3
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-01 16:59:07 -0800

    Update CarTest.java

--- src/src/test/java/Parking/CarTest.java
+++ src/src/test/java/Parking/CarTest.java
@@ -1,14 +1,14 @@
 package Parking;
 
-import static org.junit.Assert.*; 
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class CarTest {
-	
+
 	/**
-	 * Test whether the constructor works well taking in the variables,
-	 * even with empty parameters.
+	 * Test whether the constructor works well taking in the variables, even with
+	 * empty parameters.
 	 */
 	@Test
 	public void TestCarConstructor() {
@@ -16,9 +16,9 @@ public class CarTest {
 		Car newCar2 = new Car("", "1234567");
 		Car newCar3 = new Car("PA", "");
 		System.out.println(newCar1.toString());
-		
-		assertEquals("CA1234567", newCar1.getState() + newCar1.getLicense());
-		assertEquals("1234567", newCar2.getState() + newCar2.getLicense());
+
+		assertEquals("CA1234567", newCar1.state + newCar1.license);
+		assertEquals("1234567", newCar2.state + newCar2.license);
 		assertEquals("PA", newCar3.getState() + newCar3.getLicense());
 	}
 
