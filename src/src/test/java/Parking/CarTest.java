e94d39445630ae3d0cf3d578a61a320f5726c025
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-01 17:04:22 -0800

    Update CarTest.java

--- src/src/test/java/Parking/CarTest.java
+++ src/src/test/java/Parking/CarTest.java
@@ -1,25 +1,37 @@
 package Parking;
 
-import static org.junit.Assert.*; 
+import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CarTest {
-	
+
+	Car newCar1 = new Car("CA", "1234567");
+	Car newCar2 = new Car("", "1234567");
+	Car newCar3 = new Car("PA", "");
+
 	/**
-	 * Test whether the constructor works well taking in the variables,
-	 * even with empty parameters.
+	 * Test whether the constructor works well taking in the variables, even with
+	 * empty parameters.
 	 */
 	@Test
-	public void TestConstructor() {
-		Car newCar1 = new Car("CA", "1234567");
-		Car newCar2 = new Car("", "1234567");
-		Car newCar3 = new Car("PA", "");
-		System.out.println(newCar1.toString());
-		
+	public void TestConstandGetters() {
+
 		assertEquals("CA1234567", newCar1.getState() + newCar1.getLicense());
 		assertEquals("1234567", newCar2.getState() + newCar2.getLicense());
 		assertEquals("PA", newCar3.getState() + newCar3.getLicense());
+	}
+
+	/**
+	 * Test the toString method
+	 */
+	@Test
+	public void TesttoString() {
+
+		assertEquals("CA, 1234567", newCar1.toString());
 	}
 
 }
