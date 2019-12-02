1110c30a1facdc9c55392aad3ce5b7e35b223f7c
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-01 17:22:12 -0800

    Update ParkingInstanceProcessor.java

--- src/src/main/java/Parking/ParkingInstanceProcessor.java
+++ src/src/main/java/Parking/ParkingInstanceProcessor.java
@@ -22,10 +22,8 @@ public class ParkingInstanceProcessor {
 	 * @param photo
 	 * @return
 	 */
-	public ArrayList<ParkingInstance> createParkingInstances(
-			ArrayList<Photo> photos) {
-		ArrayList<ParkingInstance> parkingInstances =
-				new ArrayList<ParkingInstance>();
+	public ArrayList<ParkingInstance> createParkingInstances(ArrayList<Photo> photos) {
+		ArrayList<ParkingInstance> parkingInstances = new ArrayList<ParkingInstance>();
 		for (Photo photo : photos) {
 			Car myCar = ocr.getCarWithOpenALPR(photo);
 			if (myCar == null) {
@@ -36,25 +34,28 @@ public class ParkingInstanceProcessor {
 		return parkingInstances;
 	}
 
-	public ArrayList<ParkingInstance> createParkingInstances(Path path) throws ParkingException {
+	public ArrayList<ParkingInstance> createParkingInstances(Path path)
+			throws ParkingException {
 		ArrayList<Photo> photos = PhotoFactory.createPhotos(path);
 		return createParkingInstances(photos);
 	}
 
-	public ArrayList<ParkingInstance> createParkingInstances(List<File> files) throws ParkingException {
+	public ArrayList<ParkingInstance> createParkingInstances(List<File> files)
+			throws ParkingException {
 		ArrayList<Photo> photos = PhotoFactory.createPhotos(files);
 		return createParkingInstances(photos);
 	}
 
 	/**
-	 * This method adds multiple parking instance objects from an ArrayList to
-	 * the Database.
+	 * This method adds multiple parking instance objects from an ArrayList to the
+	 * Database.
 	 * 
 	 * @param db
 	 * @param filePath
 	 */
-	public void addParkingInstancesToDB(Database db,
-			ArrayList<ParkingInstance> parkingInstances) throws ParkingException {
+	public void addParkingInstancesToDB(Database db, 
+			ArrayList<ParkingInstance> parkingInstances)
+			throws ParkingException {
 		for (ParkingInstance pi : parkingInstances) {
 			db.insertParkingInstance(pi);
 		}
@@ -74,13 +75,11 @@ public class ParkingInstanceProcessor {
 		return parkings;
 	}
 
-
 	public static void main(String[] args) {
-		Path filePath =
-				Paths.get("src/test/java/Parking/MultipleImagesFolder/");
+		Path filePath = Paths.get("src/test/java/Parking/MultipleImagesFolder/");
 		try {
 			ParkingInstanceProcessor pip = new ParkingInstanceProcessor();
-			ArrayList<ParkingInstance> parkingInstances =
+			ArrayList<ParkingInstance> parkingInstances = 
 					pip.createParkingInstances(filePath);
 			for (ParkingInstance parkingInstance : parkingInstances) {
 				System.out.println(parkingInstance);
