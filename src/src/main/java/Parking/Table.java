f84954e1d4f1e6fb94508083b39f31d40ba4234e
clairelua <clairelua8@gmail.com>
2019-12-01 18:34:55 -0700

    Refactor ParkingInstances TableView into its own class

--- src/src/main/java/Parking/Table.java
+++ src/src/main/java/Parking/Table.java
@@ -1,32 +1,23 @@
 package Parking;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.List;
 
 import javafx.application.Application;
-import java.time.format.DateTimeFormatter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import java.awt.image.BufferedImage;
-import javafx.embed.swing.SwingFXUtils;
-
 
 public class Table extends Application {
     private ParkingController parkingController = new ParkingController();
@@ -49,7 +40,7 @@ public class Table extends Application {
 
     public Scene tableScene() {
         Scene scene = new Scene(new Group());
-        
+
         final Label label = new Label("Parking Aggregates");
         label.setFont(new Font("Arial", 20));
 
@@ -66,7 +57,7 @@ public class Table extends Application {
     private void constructTable() {
         TableColumn<ParkingAggregate, String> state =
                 new TableColumn<ParkingAggregate, String>("State");
-        
+
         state.setMinWidth(250);
         state.setCellValueFactory(
                 new PropertyValueFactory<ParkingAggregate, String>("state"));
@@ -131,80 +122,15 @@ public class Table extends Application {
     private TableView<ParkingInstance> constructSubTable(
             ParkingAggregate parkingAggregate) {
 
-        List<ParkingInstance> parkingInstances =
-                parkingAggregate.getParkingInstances();
-        System.out.println(parkingInstances.get(0));
-
-        TableView<ParkingInstance> subTable = new TableView<ParkingInstance>();
-        TableColumn<ParkingInstance, LocalDateTime> dateCol =
-                new TableColumn<ParkingInstance, LocalDateTime>("Date Time");
-        dateCol.setMinWidth(200);
-        dateCol.setCellValueFactory(
-                new PropertyValueFactory<ParkingInstance, LocalDateTime>(
-                        "dateTime"));
-        dateCol.setCellFactory(column -> {
-            TableCell<ParkingInstance, LocalDateTime> cell =
-                    new TableCell<ParkingInstance, LocalDateTime>() {
-                        final DateTimeFormatter formatter = DateTimeFormatter
-                                .ofPattern("yyyy-MM-dd HH:mm:ss");
-
-                        @Override
-                        protected void updateItem(LocalDateTime date,
-                                boolean empty) {
-                            super.updateItem(date, empty);
-                            if (empty) {
-                                setText(null);
-                            } else {
-                                this.setText(date.format(formatter));
-
-                            }
-                        }
-                    };
-
-            return cell;
-        });
-        TableColumn<ParkingInstance, BufferedImage> photoCol =
-                new TableColumn<ParkingInstance, BufferedImage>("Photo");
-        photoCol.setMinWidth(400);
-        photoCol.setCellValueFactory(
-                new PropertyValueFactory<ParkingInstance, BufferedImage>(
-                        "thumbnail"));
-        photoCol.setCellFactory(column -> {
-            TableCell<ParkingInstance, BufferedImage> cell =
-                    new TableCell<ParkingInstance, BufferedImage>() {
-                        @Override
-                        protected void updateItem(BufferedImage image,
-                                boolean empty) {
-                            super.updateItem(image, empty);
-                            setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-                            ImageView imageView = new ImageView();
-                            if (empty || image == null) {
-                                imageView.setImage(null);
-                                setText(null);
-                                setGraphic(null);
-                                return;
-                            }
-
-                            imageView.setImage(SwingFXUtils.toFXImage(image, null));
-                            setGraphic(imageView);
-                        }
-                    };
-
-            return cell;
-        });
-
-
-        subTable.setItems(FXCollections.observableArrayList(parkingInstances));
-        subTable.getColumns().addAll(Arrays.asList(dateCol, photoCol));
-        subTable.setPrefHeight(50 + (parkingInstances.size() * 270));
-        subTable.setStyle("-fx-border-color: #42bff4;");
-        return subTable;
+        return TableViewFactory.createParkingInstancesTable(
+                parkingAggregate.getParkingInstances(),
+                false /* exclude car columns */);
     }
 
 
     private ObservableList<ParkingAggregate> getData() {
-        return FXCollections.observableArrayList(
-                parkingController.getParkingAggregates(LocalDate.of(2010, 2, 11),
-                        LocalDate.of(2019, 6, 11)));
+        return FXCollections
+                .observableArrayList(parkingController.getParkingAggregates(
+                        LocalDate.of(2010, 2, 11), LocalDate.of(2019, 6, 11)));
     }
 }
