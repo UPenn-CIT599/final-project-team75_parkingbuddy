9fe99ce1b9aee2330cdc67cf7ced75824b1246a2
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-01 16:29:49 -0800

    Update GUI.java

--- src/src/main/java/Parking/GUI.java
+++ src/src/main/java/Parking/GUI.java
@@ -122,7 +122,7 @@ public class GUI extends Application {
 			// label to show the date
 			Label labelStart =
 					new Label("Choose the start date.\n(Default 30 days ago)");
-			Label labelEnd = new Label("Choose the end date.\n(Default Today)");
+			Label labelEnd = new Label("Choose the end date.\n(Default today)");
 			labelStart.setFont(Font.font("Verdana", 15));
 			labelStart.setTextFill(Color.web("#ffff"));
 			labelEnd.setFont(Font.font("Verdana", 15));
@@ -200,7 +200,7 @@ public class GUI extends Application {
 					popupwindow.initModality(Modality.WINDOW_MODAL);
 					popupwindow.setTitle("Parking Aggregates");
 					popupwindow.setScene(parkingTable.tableScene());
-					popupwindow.show();;
+					popupwindow.show();
 				}
 			});
 
