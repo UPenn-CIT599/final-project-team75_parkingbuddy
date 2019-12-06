3771f1f35af0a6d1ede2a0f5c6ebdb2347c616f6
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-06 12:08:22 -0800

    Update ParkingInstance.java

--- src/src/main/java/Parking/ParkingInstance.java
+++ src/src/main/java/Parking/ParkingInstance.java
@@ -5,54 +5,104 @@ import java.awt.image.BufferedImage;
 
 
 /**
- * ParkingInstance contains parking instance object that represents each instance that a car is
- * parked at the parking lot.
+ * ParkingInstance class contains parking instance object that represents each instance
+ * that a car is parked at the parking lot.
  */
 public class ParkingInstance {
+    // instance variables
 	Car car;
 	Photo photo;
-
+	
+	/**
+	 * Constructor for the ParkingInstance class
+	 * @param car (Car)
+	 * @param photo (Photo)
+	 */
 	public ParkingInstance(Car car, Photo photo) {
 		this.car = car;
 		this.photo = photo;
 	}
-
+	
+	/**
+	 * Getter method for the date and time from the photo instance variable
+	 * @return date and time (LocalDateTime)
+	 */
 	public LocalDateTime getDateTime() {
 		return photo.getDateTime();
 	}
-
+	
+	/**
+	 * Getter method for the car instance variable
+	 * @return car (Car)
+	 */
 	public Car getCar() {
 		return car;
 	}
-
+	
+	/**
+	 * Getter method for the state value from the car instance variable
+	 * @return state (String)
+	 */
 	public String getState() {
 		return car.getState();
 	}
-
+	
+	/**
+	 * Getter method for the license value from the car instance variable
+	 * @return license (String)
+	 */
 	public String getLicense() {
 		return car.getLicense();
 	}
-
+	
+	/**
+	 * Setter method for the car instance variable
+	 * @param car (Car)
+	 */
 	public void setCar(Car car) {
 		this.car = car;
 	}
-
+	
+	/**
+	 * Getter method for the MD5Hash value of the photo instance variable
+	 * @return Md5Hash (String)
+	 */
 	public String getPhotoMd5Hash() {
 		return photo.getMd5Hash();
 	}
 
+	/**
+	 * toString method that overrides the default method by concatenating
+	 * car and string instance variables with a comma and space in between
+	 * @return concatenated String
+	 */
+	@Override
 	public String toString() {
 		return car.toString() + ", " + photo.toShortString();
 	}
-
+	
+	/**
+	 * Getter method for the thumbnail image from the photo instance variable
+	 * @return thumbnail (BufferedImage)
+	 */
 	public BufferedImage getThumbnail() {
 		return photo.getThumbnail();
 	}
-
+	
+	/**
+	 * Getter method for the photo instance variable
+	 * @return photo (Photo)
+	 */
 	public Photo getPhoto() {
 		return photo;
 	}
-
+	
+	/**
+	 * equals method that overrides the default. Compares instance variables from
+	 * two ParkingInstance objects.
+	 * @param o (Object)
+	 * @return boolean
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) {
