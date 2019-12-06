dbfe87a59fcb8a58f4c50c84ce934f92676867c3
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-06 14:55:28 -0800

    Update Main.java

--- src/src/main/java/Parking/Main.java
+++ src/src/main/java/Parking/Main.java
@@ -2,7 +2,7 @@ package Parking;
 
 /**
  * This class calls on the GUI class to start the program and launch the window.
- *
+ * Does not contain any methods nor instance variables. Just calls on main.
  */
 public class Main {
     public static void main(String[] args) {
