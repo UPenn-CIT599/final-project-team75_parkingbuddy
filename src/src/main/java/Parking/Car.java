cb967fb0a72297a9de1d8d3b2f01eb474f4c4507
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-05 23:23:36 -0800

    Update Car.java

--- src/src/main/java/Parking/Car.java
+++ src/src/main/java/Parking/Car.java
@@ -1,9 +1,9 @@
 package Parking;
 
 /**
- * This is the Car class is for each car object, representing a single car. A Car object has 2
- * parameters: (1) License plate number (license) (2) State under which is car is registered, e.g.
- * California = "CA" (state)
+ * This is the Car class and each car object represents a single car. A Car object has 2
+ * instance variables: (1) License plate number (license) (2) State under which is car
+ * is registered, e.g. California = "CA" (state)
  * 
  */
 
@@ -12,10 +12,10 @@ public class Car {
     private String license;
 
     /**
-     * Constructor for Car
+     * Constructor for Car class
      * 
-     * @param license
-     * @param state
+     * @param license (String)
+     * @param state (String)
      */
     public Car(String state, String license) {
         this.state = state;
@@ -25,26 +25,39 @@ public class Car {
     /**
      * Getter method to get the car license plate number
      * 
-     * @return
+     * @return license (String)
      */
     public String getLicense() {
         return license;
     }
 
     /**
-     * Getter method to get the state that the car is registered under (e.g. California would be
-     * "CA")
+     * Getter method to get the state that the car is registered under
+     * (e.g. California would be "CA")
      * 
-     * @return
+     * @return state (String)
      */
     public String getState() {
         return state;
     }
-
+    
+    /**
+     * Method that concatenates state and license with a comma and space.
+     * 
+     * @return String
+     */
+    @Override
     public String toString() {
         return state + ", " + license;
     }
-
+    
+    /**
+     * Method that compares between two Car objects by comparing their
+     * state and license values.
+     * 
+     * @param o (Object)
+     * @return boolean
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
