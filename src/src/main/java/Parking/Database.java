8925b1f4b46fc61e0585c26183f18f89412b86b6
minschoi7 <57383521+minschoi7@users.noreply.github.com>
2019-12-05 22:30:01 -0800

    Revert "Revert "Updates""
    This reverts commit 0dc2aebf72a2f0454826e52a059a99e661ab84d4.

--- src/src/main/java/Parking/Database.java
+++ src/src/main/java/Parking/Database.java
@@ -16,6 +16,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+/**
+ * This database class 
+ *
+ */
 public class Database {
 	final static DateTimeFormatter dateTimeFormatter =
 			DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
